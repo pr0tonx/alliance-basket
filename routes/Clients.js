a98c5495b8a567dffc7a0524ac3ec66f6b2d1518
GFreitasW <gfreitas@wiley.com>
2024-04-26 17:10:04 -0300

    chore

--- routes/Clients.js
+++ routes/Clients.js
@@ -8,7 +8,7 @@ const authMiddleware = require('../middleware/authMiddleware');
 const router = express.Router()
 
 router.post('/', authMiddleware.isAuth, async (req, res) => clientController.createClient(req, res));
-router.get('/', authMiddleware.isAuth,  async (req, res) => clientController.getClients(req, res));
+router.get('/', authMiddleware.isAuth, async (req, res) => clientController.getClients(req, res));
 router.get('/:id', authMiddleware.isAuth, async (req, res) => clientController.getClientById(req, res));
 // FIX: https://trello.com/c/nG8WJd64/15-fix-updateusers-try-to-update-everything
 router.put('/:id', authMiddleware.isAuth, async (req, res) => clientController.updateClient(req, res));
