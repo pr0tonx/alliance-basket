67ca51290ba7a5e3bc7d7dfaeff463ddd8c1d292
Felipe <felipepeengler@outlook.com>
2024-05-28 15:47:40 -0300

    feat api to return members from group;

--- routes/Member.js
+++ routes/Member.js
@@ -6,6 +6,7 @@ const authMiddleware = require('../middleware/authMiddleware');
 const router = express.Router();
 
 router.post('/clients/:id/groups', authMiddleware.isAuth, (req, res) => membersController.addMember(req, res));
+router.get('/groups/:idGroup', authMiddleware.isAuth, (req, res) => membersController.getMembersFromGroup(req, res));
 router.delete('/groups/:idGroup/clients/:idClient', authMiddleware.isAuth, (req, res) => membersController.removeMember(req, res));
 
 // router.get('/group/:id', (req, res) => membersController.listAllMembers(req, res));
